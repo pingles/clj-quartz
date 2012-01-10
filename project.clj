5f040ce17deb1372bacbf3d40a909db8122051d4
Paul Ingles <paul@oobaloo.co.uk>
2012-01-10 22:58:10 +0000

    add basics to start + shutdown the scheduler

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
-(defproject clj-quartz "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+(defproject clj-quartz "0.0.1-SNAPSHOT"
+  :description ""
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/tools.logging "0.2.3"]
+                 [org.quartz-scheduler/quartz "2.1.2"]]
+  :dev-dependencies [[org.slf4j/slf4j-simple "1.6.1"]])
--- src/clj_quartz/core.clj
+++ src/clj_quartz/core.clj
@@ -1,1 +1,44 @@
-(ns clj-quartz.core)
+(ns clj-quartz.core
+  (:import [org.quartz.impl StdSchedulerFactory]
+           [org.quartz Scheduler]
+           [java.util Properties]))
+
+(def defaults {"org.quartz.scheduler.instanceName" "Testing"
+               "org.quartz.threadPool.threadCount" "1"
+               "org.quartz.jobStore.class" "org.quartz.simpl.RAMJobStore"})
+
+(defn as-properties
+  [m]
+  (let [p (Properties.)]
+    (doseq [[k v] m]
+      (.setProperty p (name k) (name v)))
+    p))
+
+(defn create-scheduler
+  "See defaults for examples."
+  [config]
+  (.getScheduler (doto (StdSchedulerFactory.)
+                   (.initialize (as-properties config)))))
+
+(defn start
+  [^Scheduler scheduler]
+  (.start scheduler))
+
+(defn shutdown
+  [^Scheduler scheduler]
+  (.shutdown scheduler))
+
+(defn metadata
+  [^Scheduler scheduler]
+  (let [data (.getMetaData scheduler)]
+    {:job-store (.getJobStoreClass data)
+     :scheduler (.getSchedulerClass data)
+     :instance-id (.getSchedulerInstanceId data)
+     :name (.getSchedulerName data)
+     :summary (.getSummary data)
+     :thread-pool (.getThreadPoolClass data)
+     :thread-pool-size (.getThreadPoolSize data)
+     :version (.getVersion data)
+     :standby-mode (.isInStandbyMode data)
+     :shutdown (.isShutdown data)
+     :started (.isStarted data)}))
