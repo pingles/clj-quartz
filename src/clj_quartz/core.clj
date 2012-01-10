8c61206c731ccea0e3a00b7ec66ed269e53377d6
Paul Ingles <paul@oobaloo.co.uk>
2012-01-10 21:43:18 +0000

    initial commit

---
+++ src/clj_quartz/core.clj
@@ -0,0 +1,1 @@
+(ns clj-quartz.core)
---
+++ test/clj_quartz/test/core.clj
@@ -0,0 +1,6 @@
+(ns clj-quartz.test.core
+  (:use [clj-quartz.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
