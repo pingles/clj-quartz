8791fac19b0bbd65f36b0a501afc8a9bd6b22864
Paul Ingles <paul@oobaloo.co.uk>
2012-01-11 09:52:25 +0000

    initial autodoc

---
+++ index.clj
@@ -0,0 +1,36 @@
+{:namespaces
+ ({:source-url nil,
+   :wiki-url "clj-quartz.core-api.html",
+   :name "clj-quartz.core",
+   :author "Paul Ingles",
+   :doc "Utility functions used in clj-quartz."}
+  {:source-url nil,
+   :wiki-url "clj-quartz.example.simple-api.html",
+   :name "clj-quartz.example.simple",
+   :doc nil}
+  {:source-url nil,
+   :wiki-url "clj-quartz.job-api.html",
+   :name "clj-quartz.job",
+   :doc nil}
+  {:source-url nil,
+   :wiki-url "clj-quartz.scheduler-api.html",
+   :name "clj-quartz.scheduler",
+   :author "Paul Ingles",
+   :doc "Functions to creating and controlling Quartz Schedulers."}
+  {:source-url nil,
+   :wiki-url "clj-quartz.test-utils-api.html",
+   :name "clj-quartz.test-utils",
+   :doc nil}),
+ :vars
+ ({:arglists ([config]),
+   :name "create-scheduler",
+   :namespace "clj-quartz.scheduler",
+   :source-url nil,
+   :raw-source-url nil,
+   :wiki-url
+   "/clj-quartz.scheduler-api.html#clj-quartz.scheduler/create-scheduler",
+   :doc
+   "Creates a scheduler with the provided configuration (a sequence of\nproperty name/values).",
+   :var-type "function",
+   :line 8,
+   :file "src/clj_quartz/scheduler.clj"})}
