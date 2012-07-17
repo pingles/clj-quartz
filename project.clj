0a4db8a3372832741f3e076f13fc0b79c66da6a1
Paul Ingles <paul@oobaloo.co.uk>
2012-07-17 15:12:19 +0100

    ignore target + changes for lein 2

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject clj-quartz "0.0.1-SNAPSHOT"
-  :description ""
+(defproject clj-quartz/clj-quartz "0.0.1-SNAPSHOT" 
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/tools.logging "0.2.3"]
                  [org.quartz-scheduler/quartz "2.1.2"]]
-  :dev-dependencies [[org.slf4j/slf4j-nop "1.6.1"]]
-  :source-path "src/clojure"
-  :java-source-path "src/java")
+  :source-paths ["src/clojure"]
+  :profiles {:dev {:dependencies [[org.slf4j/slf4j-nop "1.6.1"]]}}
+  :java-source-paths ["src/java"]
+  :min-lein-version "2.0.0"
+  :description "")
