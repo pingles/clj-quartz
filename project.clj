13be4e73a5656181aa4d924270c0caee6bdcfb09
Paul Ingles <paul@oobaloo.co.uk>
2013-05-10 18:30:32 +0100

    roll back to 1.4.0- broken in my dev clojure emacs setup for now; log to console in development, nop any log messages when running tests

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject clj-quartz/clj-quartz "0.0.1-SNAPSHOT" 
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.logging "0.2.6"]
                  [org.quartz-scheduler/quartz "2.1.2"]]
   :source-paths ["src/clojure"]
-  :profiles {:dev {:dependencies [[org.slf4j/slf4j-nop "1.6.1"]]}}
+  :profiles {:dev {:dependencies [[org.slf4j/slf4j-simple "1.6.1"]]}
+             :test {:dependencies [[org.slf4j/slf4j-nop "1.6.1"]]}}
   :java-source-paths ["src/java"]
   :min-lein-version "2.0.0"
   :description "")
